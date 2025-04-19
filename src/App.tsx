3a80a5da2fc4306833d66cd79d94809db739de37
Ali Shaikh <shahinshaikh603@gmail.com>
2025-04-19 23:18:26 +0530

    refactor: extract inline styles in App.tsx into variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import React, {useState} from 'react'
+import { useState } from 'react';
+import words from './wordlList.json';
+import HangmanDrawing from './HangmanDrawing';
+import HangmanWord from './HangmanWord';
+import Keyboard from './Keyboard';
+
+const containerStyle: React.CSSProperties = {
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center",
+};
+
+const titleStyle: React.CSSProperties = {
+    fontSize: "2rem",
+    textAlign: "center",
+};
 
 const App = () => {
-    const [wordToGuess, setWordToGuess] = useState("Test");
+    const [wordToGuess, setWordToGuess] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)];
+    });
+
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
     return (
-        <div>App</div>
-    )
-}
-export default App
+        <div style={containerStyle}>
+            <div style={titleStyle}>Lose Win</div>
+            <HangmanDrawing />
+            <HangmanWord />
+            <Keyboard />
+        </div>
+    );
+};
 
-
-
-
-
-
-
-
-
-
-
+export default App;
